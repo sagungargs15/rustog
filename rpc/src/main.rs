ef03e923988cfb25ba89028199a0746acd735a7c
Sagun Garg <sagun.garg@juliusbaer.com>
2025-01-18 15:54:53 +0800

    updated assignment 002 inside rpc-scavenger

--- rpc/src/main.rs
+++ rpc/src/main.rs
@@ -9,26 +9,90 @@
 
 use anyhow::Result;
 use serde_json::{json, Value}; // allow you to use the json! macro for creating JSON values.
+use reqwest::Client;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let url = "http://boss2025.xyz:8332";
-    let client = reqwest::Client::new();
+    let client = Client::new();
+    let user = "user_324";
+    let password = "Rn0tEynYp6tV";
 
-    let response = client
+    let response_001 = client
+    .post(url)
+    .basic_auth(user, Some(password))
+    .header("Content-Type", "application/json")
+    .json(&json!({
+        "method": "getblockcount",
+        "jsonrpc": "1.0",
+        "id": "1"
+    }))
+    .send()
+    .await?;
+
+    let result_001: Value = response_001.json().await?;
+    println!("Response_001: {:?}", serde_json::to_string_pretty(&result_001)?);
+
+
+    // Step 1: Get the block hash for block 123456
+    let response_002_blockhash = client
         .post(url)
-        .basic_auth("user_324", Some("Rn0tEynYp6tV"))
+        .basic_auth(user, Some(password))
         .header("Content-Type", "application/json")
         .json(&json!({
-            "method": "getblockcount",
+            "method": "getblockhash",
+            "params": [123456],
             "jsonrpc": "1.0",
             "id": "1"
         }))
         .send()
         .await?;
 
-    let result: Value = response.json().await?;
-    println!("Response: {:?}", result);
+    let result_002_blockhash: Value = response_002_blockhash.json().await?;
+    println!("Response_002_blockhash: {:?}", serde_json::to_string_pretty(&result_002_blockhash)?);
+    let block_hash = result_002_blockhash["result"].as_str().unwrap();
+
+    // Step 2: Get the block data using the block hash
+    let response_002_blockdata = client
+        .post(url)
+        .basic_auth(user, Some(password))
+        .header("Content-Type", "application/json")
+        .json(&json!({
+            "method": "getblock",
+            "params": [block_hash],
+            "jsonrpc": "1.0",
+            "id": "1"
+        }))
+        .send()
+        .await?;
+
+    let result_002_blockdata: Value = response_002_blockdata.json().await?;
+    println!("Response_002_blockdata: {:?}", serde_json::to_string_pretty(&result_002_blockdata)?);    
+    let transactions = result_002_blockdata["result"]["tx"].as_array().unwrap();
+    println!("Transactions: {:?}", transactions);
+
+    // Step 3: Count the outputs in each transaction
+    let mut total_outputs = 0;
+    for txid in transactions {
+        let tx_data_response = client
+            .post(url)
+            .basic_auth(user, Some(password))
+            .header("Content-Type", "application/json")
+            .json(&json!({
+                "method": "getrawtransaction",
+                "params": [txid, true],
+                "jsonrpc": "1.0",
+                "id": "1"
+            }))
+            .send()
+            .await?;
+
+        let tx_data_result: Value = tx_data_response.json().await?;
+        let outputs = tx_data_result["result"]["vout"].as_array().unwrap();
+        total_outputs += outputs.len();
+    }
+    
+    println!("Total new outputs in block 123,456: {}", total_outputs);
 
     Ok(())
 }
